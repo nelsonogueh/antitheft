09d783e74d4e16140e6a5dc3909121ad0ba0b56a
nelson <nelsonovovwe@gmail.com>
2019-05-20 17:28:23 +0100

    Working well on device, but background service for internet-related work is commented out

--- Anti Theft/src/main/java/com/dulceprime/antitheft/services/Background_Service.java
+++ Anti Theft/src/main/java/com/dulceprime/antitheft/services/Background_Service.java
@@ -499,7 +499,7 @@ public class Background_Service extends Service {
 
             if (s.trim().equalsIgnoreCase("OK")) { // Message sent
 //                prefManager.setIsReportSent(true);
-                prefManager.setIsFreshRequest(false);
+//                prefManager.setIsFreshRequest(false);
                 Toast.makeText(Background_Service.this, "SENT", Toast.LENGTH_LONG).show();
             }
 
